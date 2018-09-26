af726612c718af78363627980c97c7f998f26aff
ness <nessa.lauren5@gmail.com>
2018-09-25 21:41:05 -0700

    add static artifacts

---
+++ src/main/java/io/vloggrapp/config/spring/MVCWebAppInitializer.java
@@ -0,0 +1,21 @@
+package io.vloggrapp.config.spring;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MVCWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
--- src/main/java/io/vloggrapp/controller/ViewController.java
+++ src/main/java/io/vloggrapp/controller/ViewController.java
@@ -3,6 +3,7 @@ package io.vloggrapp.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +13,8 @@ public class ViewController {
 
     public static final Logger logger = LoggerFactory.getLogger(ViewController.class);
 
-    @RequestMapping("/")
-    public String homePage() {
+    @GetMapping("/")
+    public String homePage(Model model) {
         logger.debug("Trying to process index page.");
         return "index";
     }
